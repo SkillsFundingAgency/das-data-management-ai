3c36d7107f122e38a15e296bfc6dcfa19fb9fc70
Ewan Noble <EwanNoble@users.noreply.github.com>
2021-10-21 16:42:42 +0100

    Fix example file

--- models/placeholder/train.py
+++ models/placeholder/train.py
@@ -2,16 +2,11 @@ import pickle
 import os
 from azureml.core.run import Run
 
-from utils import mylib
-
 os.makedirs('./outputs', exist_ok=True)
 
-
 run = Run.get_context()
-
-
-
+run.log('thest log', 'test log')
 # Save model in the outputs folder so it automatically get uploaded when running on AML Compute
 model_file_name = 'output.pkl'
 with open(os.path.join('./outputs/', model_file_name), 'wb') as file:
-    pickle.dump(reg, file)
+    pickle.dump('placeholder', file)
