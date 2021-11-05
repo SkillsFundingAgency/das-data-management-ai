102cbcdb0d3c60e8230ab13a37ec03a99e718917
robertsonrichard354 <93527142+robertsonrichard354@users.noreply.github.com>
2021-11-05 11:57:26 +0000

    Add files via upload

---
+++ models/placeholder/python_feature_creation.py
@@ -0,0 +1,34 @@
+# set up libraries
+import pandas as pd
+
+# set up the connection string
+# conn = 
+#read the data from sql
+df = pd.read_sql('SELECT * from test_data',conn)
+
+# create some additional features on the data
+def fn_age_grp(row):
+    if row['dep_var2']<=30:
+        val='A_0_30'
+    elif row['dep_var2']<=60:
+        val='B_31_60'
+    else:
+        val='C_61+'
+    return val
+
+# create some additional features on the data
+def fn_age_grp(row):
+    if row['dep_var2']<=30:
+        val='A_0_30'
+    elif row['dep_var2']<=60:
+        val='B_31_60'
+    else:
+        val='C_61+'
+    return val
+
+# apply feature functions
+# create age grouping variable
+df['grp_age']=df.apply(fn_age_grp,axis=1)
+
+# create sector code from TCN
+df['sector']=df['dep_var1'].str.slice(1,3)
