a3418bfbc53f422329478a279dc8c68b91407e2f
robertsonrichard354 <93527142+robertsonrichard354@users.noreply.github.com>
2021-11-05 15:58:11 +0000

    Update train.py

--- models/placeholder/train.py
+++ models/placeholder/train.py
@@ -4,7 +4,12 @@ from azureml.core.run import Run
 from azureml.core import Datastore
 
 aml_workspace = Run.get_context().experiment.workspace
-datastore = Datastore.get(aml_workspace, datastore_name='trainingdata')
+# datastore = Datastore.get(aml_workspace, datastore_name='trainingdata')
+
+datastore = Datastore.get(aml_workspace, datastore_name='datamgmtdb')
+query = DataPath(datastore, 'SELECT * FROM Stg.AI_TestData')
+tabular = Dataset.Tabular.from_sql_query(query, query_timeout=10)
+df = tabular.to_pandas_dataframe()
 
 os.makedirs('./outputs', exist_ok=True)
 
@@ -12,6 +17,7 @@ run = Run.get_context()
 run.log('test log', 'test log')
 
 # Save model in the outputs folder so it automatically get uploaded when running on AML Compute
-model_file_name = 'output.pkl'
-with open(os.path.join('./outputs/', model_file_name), 'wb') as file:
-    pickle.dump('placeholder', file)
+# model_file_name = 'output.pkl'
+# with open(os.path.join('./outputs/', model_file_name), 'wb') as file:
+#     pickle.dump('placeholder', file)
+
