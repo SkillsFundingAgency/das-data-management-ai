4c6d93a6e02e099bf2d26faec7a26698d8391684
robertsonrichard354 <93527142+robertsonrichard354@users.noreply.github.com>
2021-11-16 13:24:52 +0000

    Update train.py

--- models/placeholder/train.py
+++ models/placeholder/train.py
@@ -3,7 +3,7 @@ from azureml.core import Workspace, Datastore, Dataset, ComputeTarget, Experimen
 from azureml.core.run import Run
 from azureml.pipeline.steps import PythonScriptStep
 from azureml.pipeline.core import Pipeline, PipelineData
-
+ 
 ws = Run.get_context().experiment.workspace
 compute_target = ws.compute_targets["cpucluster"]
 experiment = Experiment(ws, 'placeholder-exp') 
