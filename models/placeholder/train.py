a4de3295bc949e04d5077506a224b84cef9647e0
robertsonrichard354 <93527142+robertsonrichard354@users.noreply.github.com>
2021-11-16 10:37:43 +0000

    Update train.py

--- models/placeholder/train.py
+++ models/placeholder/train.py
@@ -1,51 +1,20 @@
-import pickle
-import os
-import pandas as pd
-import pyarrow.parquet as pq
+import azureml.core
+from azureml.core import Workspace, Datastore, Dataset
 from azureml.core.run import Run
-from azureml.core import Dataset, Datastore
-from azureml.data.datapath import DataPath
-from sklearn.linear_model import LogisticRegression
 
-aml_workspace = Run.get_context().experiment.workspace
-# datastore = Datastore.get(aml_workspace, datastore_name='trainingdata')
+ws = Run.get_context().experiment.workspace
+compute_target = ws.compute_targets["STANDARD_NC6"]
+experiment = Experiment(ws, 'placeholder-exp') 
 
-datastore = Datastore.get(aml_workspace, datastore_name='datamgmtdb')
-query = DataPath(datastore, 'SELECT * FROM Stg.AI_TestData')
-tabular = Dataset.Tabular.from_sql_query(query, query_timeout=10)
-df = tabular.to_pandas_dataframe()
+score_step = PythonScriptStep(
+    name='scoring'
+    script_name="model_scoring.py",
+    target=compute_target,
+    source_directory=placeholder)
 
-os.makedirs('./outputs', exist_ok=True)
+steps = [score_step]
 
-#frequency count of target variable
-print(df.groupby(['target'])['dep_var3'].value_counts())
+pipeline = Pipeline(workspace=ws, steps=steps)
 
-#split data into target and independent sets
-y_train=df['target']
-x_train=df[['dep_var2','dep_var3']]
-
-#look at the data
-x_train.head(5)
-
-#build logistic model
-clf=LogisticRegression(random_state=0).fit(x_train,y_train)
-
-#score dataset back onto the base
-scored=clf.predict_proba(x_train)
-df['model_prediction']=scored[:,1]
-print(df.head(5))
-
-#print model coefficients
-print(clf.coef_)
-print(clf.intercept_)
-
-run = Run.get_context()
-run.log('test log', 'test log')
-
-# Save model in the outputs folder so it automatically get uploaded when running on AML Compute
-model_file_name = 'test_model.pkl'
-with open(os.path.join('./outputs/', model_file_name), 'wb') as file:
-    pickle.dump(clf, file)
-
-#write out scored file to parquet
-df.to_parquet('./outputs/test_scored_model.parquet')
+pipeline_run = experiment.submit(pipeline)
+pipeline_run.wait_for_completion()
