09d2e78793065d7d7da59ee1b312f830d6d9864b
robertsonrichard354 <93527142+robertsonrichard354@users.noreply.github.com>
2021-11-17 09:38:57 +0000

    Update train.py

--- models/placeholder/train.py
+++ models/placeholder/train.py
@@ -3,7 +3,8 @@ from azureml.core import Workspace, Datastore, Dataset, ComputeTarget, Experimen
 from azureml.core.run import Run
 from azureml.pipeline.steps import PythonScriptStep
 from azureml.pipeline.core import Pipeline, PipelineData
- 
+from azureml.pipeline.core import PublishedPipeline
+
 ws = Run.get_context().experiment.workspace
 compute_target = ws.compute_targets["cpucluster"]
 experiment = Experiment(ws, 'placeholder-exp') 
@@ -17,7 +18,12 @@ score_step = PythonScriptStep(
 
 steps = [score_step]
 
+# Create pipeline
 pipeline = Pipeline(workspace=ws, steps=steps)
+pipeline.validate()
 
-pipeline_run = experiment.submit(pipeline)
-pipeline_run.wait_for_completion()
+# Publish pipeline to AzureML
+published_pipeline = pipeline.publish('model-scoring-pipeline')
+
+# pipeline_run = experiment.submit(pipeline)
+# pipeline_run.wait_for_completion()
