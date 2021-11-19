e73e7d47fa12ca8caf3231ed5ebb399ac3596a42
Ewan Noble <EwanNoble@users.noreply.github.com>
2021-11-19 12:28:09 +0000

    Clean up initial pipeline script

--- ml_service/pipelines/employer_engagement_build_pipeline.py
+++ ml_service/pipelines/employer_engagement_build_pipeline.py
@@ -1,8 +1,4 @@
-from azureml.pipeline.core.graph import PipelineParameter
-from azureml.pipeline.steps import PythonScriptStep
-from azureml.pipeline.core import Pipeline, PipelineData
-from azureml.core import Workspace, Dataset, Datastore
-from azureml.core.runconfig import RunConfiguration
+from azureml.core import Workspace
 import os
 
 
@@ -13,7 +9,8 @@ def main():
     ws = Workspace.get(
         name=workspace_name,
         subscription_id=subscription_id,
-        resource_group=resource_group,
+        resource_group=resource_group
+    )
     
     print("get_workspace:")
     print(ws)
