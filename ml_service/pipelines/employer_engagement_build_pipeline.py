3266c167b3ade4c245f89d54052520cedb4c3e97
Ewan Noble <EwanNoble@users.noreply.github.com>
2021-11-19 12:19:49 +0000

    Env vars instead of config file

--- ml_service/pipelines/employer_engagement_build_pipeline.py
+++ ml_service/pipelines/employer_engagement_build_pipeline.py
@@ -7,7 +7,16 @@ import os
 
 
 def main():
-    ws = Workspace.from_config()
+    subscription_id = os.environ.get("SUBSCRIPTION_ID")
+    workspace_name = os.environ.get("WORKSPACE_NAME")
+    resource_group = os.environ.get("RESOURCE_GROUP")
+    ws = Workspace.get(
+        name=workspace_name,
+        subscription_id=subscription_id,
+        resource_group=resource_group,
+    
+    print("get_workspace:")
+    print(ws)
 
 if __name__ == "__main__":
     main()
