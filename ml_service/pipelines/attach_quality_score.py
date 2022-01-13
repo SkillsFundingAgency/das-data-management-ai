e55d6f52ac7e507b4be75b3af07e7988e24b74ca
robertsonrichard354 <robertsonrichard@hotmail.com>
2022-01-13 16:53:43 +0000

    Additional changes to imports

--- ml_service/pipelines/attach_quality_score.py
+++ ml_service/pipelines/attach_quality_score.py
@@ -5,6 +5,51 @@ from azureml.pipeline.steps import PythonScriptStep
 from azureml.pipeline.core import Pipeline, PipelineData, StepSequence, PublishedPipeline
 from azureml.core.runconfig import RunConfiguration
 
+import azureml.core
+import os
+from azureml.core import Workspace, Datastore, Dataset, ComputeTarget, Experiment, ScriptRunConfig, Environment, Model
+from azureml.core.run import Run
+from ml_service.util.manage_environment import get_environment
+
+
+# Set up all environment details
+e = Env()
+subscription_id = os.environ.get("SUBSCRIPTION_ID")
+workspace_name = os.environ.get("WORKSPACE_NAME")
+resource_group = os.environ.get("RESOURCE_GROUP")
+
+# Get Azure machine learning workspace
+aml_workspace = Workspace.get(
+name=e.workspace_name,
+subscription_id=e.subscription_id,
+resource_group=e.resource_group,
+)
+print("get_workspace:")
+print(aml_workspace)
+
+# Create experiment if it doesn't exist
+experiment = Experiment(aml_workspace, "employer-engagement")
+
+# Add compute information to variable
+aml_compute = aml_workspace.compute_targets["cpucluster"]
+
+# Set up experiment folder
+experiment_folder = 'employer-engagement'
+
+# Create a Python environment for the experiment (from a .yml file)
+environment = Environment.from_conda_specification("environment", experiment_folder + "/conda_dependencies.yml")
+# Register the environment 
+environment.register(workspace=aml_workspace)
+registered_env = Environment.get(aml_workspace, 'environment')
+# Create a new runconfig object for the pipeline
+pipeline_run_config = RunConfiguration()
+# Use the compute you created above. 
+pipeline_run_config.target = aml_compute
+# Assign the environment to the run configuration
+pipeline_run_config.environment = registered_env
+print ("Run configuration created.")
+
+
 def get_quality_score() :
 
     score_source_dir="./employer-engagement/scoring"
