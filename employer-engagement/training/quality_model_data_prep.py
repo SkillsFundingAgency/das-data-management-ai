d5a31c6a886627d844513a2d9bd86c713be9e3b1
robertsonrichard354 <robertsonrichard@hotmail.com>
2022-01-26 15:09:24 +0000

    Alteration of dates for quality model

--- employer-engagement/training/quality_model_data_prep.py
+++ employer-engagement/training/quality_model_data_prep.py
@@ -133,11 +133,11 @@ FROM (SELECT A.A3 \
 , B.B1 as apprenticeship_id \
 , B.commitment_date \
 FROM \
-(SELECT A3, A1 as levy_split, A2 FROM PDS_AI.PT_A WHERE cast(A2<'2019-09-01' as date)) A \
+(SELECT A3, A1 as levy_split, A2 FROM PDS_AI.PT_A WHERE cast(A2<'2019-07-01' as date)) A \
 INNER JOIN 
 (SELECT B10, B15, B3, CAST(B2 AS DATE) AS commitment_date, B1 \
 FROM PDS_AI.PT_B \
-WHERE CAST(B2 AS DATE) >= cast('2019-01-01' as date) AND CAST(B2 AS DATE) < cast('2019-09-01' as date) \
+WHERE CAST(B2 AS DATE) >= cast('2019-01-01' as date) AND CAST(B2 AS DATE) < cast('2019-07-01' as date) \
 AND B3 IN (2,3,4,5) \
 AND (B15=1 OR B16 IS NOT NULL OR B19=1) \
 ) B \
@@ -145,7 +145,7 @@ ON A.A3=B.B10) C \
 LEFT JOIN \
 (SELECT B10, CAST(B2 AS DATE) as B2 \
 FROM PDS_AI.PT_B \
-WHERE CAST(B2 AS DATE) < cast('2019-09-01' as date) AND CAST(B2 AS DATE) >= cast('2018-01-01' as date) \
+WHERE CAST(B2 AS DATE) < cast('2019-07-01' as date) AND CAST(B2 AS DATE) >= cast('2018-01-01' as date) \
 AND B3 IN (2,3,4,5) \
 ) D \
 ON C.A3=D.B10 \
@@ -189,7 +189,7 @@ FROM \
 INNER JOIN \
 (SELECT B10, B6, B15, B3, B13, B12, CAST(B2 AS DATE) AS commitment_date, B11, B1 \
 FROM PDS_AI.PT_B \
-WHERE CAST(B2 AS DATE) >= cast('2019-07-01' as date) AND CAST(B2 AS DATE) < cast('2020-01-01' as date) \
+WHERE CAST(B2 AS DATE) >= cast('2019-07-01' as date) AND CAST(B2 AS DATE) < cast('2019-09-01' as date) \
 AND B3 IN (2,3,4,5) \
 AND (B15=1 OR B16 IS NOT NULL OR B19=1) \
 ) B \
@@ -197,7 +197,7 @@ ON A.A3=B.B10) C \
 LEFT JOIN \
 (SELECT B10, CAST(B2 AS DATE) as B2 \
 FROM PDS_AI.PT_B \
-WHERE CAST(B2 AS DATE) < cast('2020-01-01' as date) AND CAST(B2 AS DATE) >= cast('2018-07-01' as date) \
+WHERE CAST(B2 AS DATE) < cast('2019-09-01' as date) AND CAST(B2 AS DATE) >= cast('2018-07-01' as date) \
 AND B3 IN (2,3,4,5) \
 ) D \
 ON C.A3=D.B10 \
