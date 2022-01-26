bdf8eda2d61755249f0fe59c5e8d6b02ca5ee5d4
robertsonrichard354 <robertsonrichard@hotmail.com>
2022-01-26 00:09:19 +0000

    Try full code run

--- employer-engagement/training/quality_model_data_prep.py
+++ employer-engagement/training/quality_model_data_prep.py
@@ -65,11 +65,381 @@ tabular_2018_H1 = Dataset.Tabular.from_sql_query(query_2018_H1, query_timeout=10
 quality_model_set_2018_H1 = tabular_2018_H1.to_pandas_dataframe()
 
 
+# 2018 H2
+
+query_2018_H2 = DataPath(datastore, """SELECT C.A3 AS account_id \
+, C.levy_split \
+, C.account_created \
+, C.completed_commitment \
+, C.apprenticeship_id \
+, C.commitment_date \
+, SUM(CASE WHEN DATEDIFF(day,D.B2, C.commitment_date)<=365 and DATEDIFF(day,D.B2, C.commitment_date)>0 THEN 1 ELSE 0 END) AS previous_12mon_commitments \
+FROM (SELECT A.A3 \
+, A.levy_split \
+, A.A2 AS account_created \
+, B.B15 AS completed_commitment \
+, B.B1 as apprenticeship_id \
+, B.commitment_date \
+FROM \
+    (SELECT A3, A1 as levy_split, A2 \
+    FROM STG.PT_A \
+    WHERE A2<'2019-01-01' \
+    ) A  \
+INNER JOIN \ 
+    (SELECT B10, B15, B3, CAST(B2 AS DATE) AS commitment_date, B1 \
+    FROM STG.PT_B \
+    WHERE CAST(B2 AS DATE) >= '2018-07-01' AND CAST(B2 AS DATE) < '2019-01-01' \
+    AND B3 IN (2,3,4,5)  \
+	AND (B15=1 OR B16 IS NOT NULL OR B19=1) \
+    ) B  \
+ON A.A3=B.B10) C \
+LEFT JOIN  \
+    (SELECT B10, CAST(B2 AS DATE) as B2 \
+    FROM STG.PT_B \
+    WHERE CAST(B2 AS DATE) < '2019-01-01' AND CAST(B2 AS DATE) >= '2017-07-01' \
+    AND B3 IN (2,3,4,5) \
+    ) D  \
+ON C.A3=D.B10 \
+GROUP BY C.A3  \
+, C.levy_split \
+, C.account_created \
+, C.completed_commitment \
+, C.apprenticeship_id \
+, C.commitment_date \
+""")
+tabular_2018_H2 = Dataset.Tabular.from_sql_query(query_2018_H2, query_timeout=10)
+quality_model_set_2018_H2 = tabular_2018_H2.to_pandas_dataframe()
 
 
-print(quality_model_set_2018_H1)
 
 
+print(quality_model_set_2018_H2)
+
+
+
+
+
+
+# 2019 H1
+
+query_2019_H1 = DataPath(datastore, """SELECT C.A3 AS account_id \
+, C.levy_split \
+, C.account_created \
+, C.completed_commitment \
+, C.apprenticeship_id \
+, C.commitment_date \
+, SUM(CASE WHEN DATEDIFF(day,D.B2, C.commitment_date)<=365 and DATEDIFF(day,D.B2, C.commitment_date)>0 THEN 1 ELSE 0 END) AS previous_12mon_commitments \
+FROM (SELECT A.A3 \
+, A.levy_split \
+, A.A2 AS account_created \
+, B.B15 AS completed_commitment \
+, B.B1 as apprenticeship_id \
+, B.commitment_date \
+FROM \
+    (SELECT A3, A1 as levy_split, A2 \
+    FROM STG.PT_A \
+    WHERE A2<'2019-09-01' \
+    ) A  \
+INNER JOIN \ 
+    (SELECT B10, B15, B3, CAST(B2 AS DATE) AS commitment_date, B1 \
+    FROM STG.PT_B \
+    WHERE CAST(B2 AS DATE) >= '2019-01-01' AND CAST(B2 AS DATE) < '2019-07-01' \
+    AND B3 IN (2,3,4,5)  \
+	AND (B15=1 OR B16 IS NOT NULL OR B19=1) \
+    ) B  \
+ON A.A3=B.B10) C \
+LEFT JOIN  \
+    (SELECT B10, CAST(B2 AS DATE) as B2 \
+    FROM STG.PT_B \
+    WHERE CAST(B2 AS DATE) < '2019-07-01' AND CAST(B2 AS DATE) >= '2018-01-01' \
+    AND B3 IN (2,3,4,5) \
+    ) D  \
+ON C.A3=D.B10 \
+GROUP BY C.A3  \
+, C.levy_split \
+, C.account_created \
+, C.completed_commitment \
+, C.apprenticeship_id \
+, C.commitment_date \
+""")
+tabular_2019_H1 = Dataset.Tabular.from_sql_query(query_2019_H1, query_timeout=10)
+quality_model_set_2019_H1 = tabular_2019_H1.to_pandas_dataframe()
+
+
+
+
+
+print(quality_model_set_2019_H1)
+
+
+
+
+
+# 2019 H2
+
+query_2019_H2 = DataPath(datastore, """SELECT C.A3 AS account_id \
+, C.levy_split \
+, C.account_created \
+, C.completed_commitment \
+, C.apprenticeship_id \
+, C.commitment_date \
+, SUM(CASE WHEN DATEDIFF(day,D.B2, C.commitment_date)<=365 and DATEDIFF(day,D.B2, C.commitment_date)>0 THEN 1 ELSE 0 END) AS previous_12mon_commitments \
+FROM (SELECT A.A3 \
+, A.levy_split \
+, A.A2 AS account_created \
+, B.B15 AS completed_commitment \
+, B.B1 as apprenticeship_id \
+, B.commitment_date \
+FROM \
+    (SELECT A3, A1 as levy_split, A2 \
+    FROM STG.PT_A \
+    WHERE A2<'2019-09-01' \
+    ) A  \
+INNER JOIN  \
+    (SELECT B10, B6, B15, B3, B13, B12, CAST(B2 AS DATE) AS commitment_date, B11, B1 \
+    FROM STG.PT_B \
+    WHERE CAST(B2 AS DATE) >= '2019-07-01' AND CAST(B2 AS DATE) < '2020-01-01' \
+    AND B3 IN (2,3,4,5)  \
+	AND (B15=1 OR B16 IS NOT NULL OR B19=1) \
+    ) B  \
+ON A.A3=B.B10) C \
+LEFT JOIN  \
+    (SELECT B10, CAST(B2 AS DATE) as B2 \
+    FROM STG.PT_B \
+    WHERE CAST(B2 AS DATE) < '2020-01-01' AND CAST(B2 AS DATE) >= '2018-07-01' \
+    AND B3 IN (2,3,4,5) \
+    ) D  \
+ON C.A3=D.B10 \
+GROUP BY C.A3  \
+, C.levy_split \
+, C.account_created \
+, C.completed_commitment \
+, C.apprenticeship_id \
+, C.commitment_date \
+""")
+tabular_2019_H2 = Dataset.Tabular.from_sql_query(query_2019_H2, query_timeout=10)
+quality_model_set_2019_H2 = tabular_2019_H2.to_pandas_dataframe()
+
+
+
+
+print(quality_model_set_2019_H2)
+
+
+
+
+
+
+# Create commitments plus proportion in each occupation in SQL for each cohort
+
+query_commitment_info_all = DataPath(datastore, """SELECT  \
+A.B1 as apprenticeship_id \
+, A.B3 AS apprenticeship_level \
+, A.B13 AS apprentice_age \
+, A.B12 AS funded_by_levy_transfer \
+, A.B11 AS provider_id \
+, CASE WHEN A.B6 = '1' THEN 1 ELSE 0 END AS occupation_1 \
+, CASE WHEN A.B6 = '2' THEN 1 ELSE 0 END AS occupation_2 \
+, CASE WHEN A.B6 = '3' THEN 1 ELSE 0 END AS occupation_3 \
+, CASE WHEN A.B6 = '7' THEN 1 ELSE 0 END AS occupation_7 \
+, CASE WHEN A.B6 = '13' THEN 1 ELSE 0 END AS occupation_13 \
+, CASE WHEN A.B6 = '14' THEN 1 ELSE 0 END AS occupation_14 \
+, CASE WHEN A.B6 = '15' THEN 1 ELSE 0 END AS occupation_15 \
+, CASE WHEN A.B6 = '17' THEN 1 ELSE 0 END AS occupation_17 \
+, CASE WHEN A.B6 = '20' THEN 1 ELSE 0 END AS occupation_20 \
+, CASE WHEN A.B6 = '22' THEN 1 ELSE 0 END AS occupation_22 \
+, CASE WHEN A.B6 = '24' THEN 1 ELSE 0 END AS occupation_24 \
+FROM STG.PT_B A \
+WHERE CAST(B2 AS DATE) >= '2018-01-01' AND CAST(B2 AS DATE) < '2020-01-01' \
+AND B3 IN (2,3,4,5)  \
+AND (B15=1 OR B16 IS NOT NULL) \
+""")
+tabular_commitment_info_all = Dataset.Tabular.from_sql_query(query_commitment_info_all, query_timeout=10)
+quality_commitment_info_all = tabular_commitment_info_all.to_pandas_dataframe()
+
+
+
+
+print(quality_commitment_info_all)
+
+
+
+
+
+
+# Union the four unique date ranges together
+
+quality_model_set_all=pd.concat([quality_comp_model_set_2019_H2,quality_comp_model_set_2019_H1,quality_comp_model_set_2018_H2,quality_comp_model_set_2018_H1])
+
+
+
+print(quality_model_set_all)
+
+
+
+
+# Add on the commitment info leading up to the commitment in question
+
+quality_model_set = pd.merge(quality_comp_model_set_all,  \
+                  quality_commitment_info_all, \
+                  left_on='apprenticeship_id', \
+                  right_on='apprenticeship_id', \
+                  how='left')
+
+
+
+print(quality_model_set)
+
+
+
+
+# months since apprenticeship account sign-up
+quality_model_set["months_since_sign_up"] = (pd.Timestamp(2019,9,1) - pd.to_datetime(quality_model_set["account_created"]))/ np.timedelta64(1, "M")
+
+# make the months since sign-up discrete for analysis purposes
+quality_model_set["months_since_sign_up2"] = quality_model_set["months_since_sign_up"].apply(np.floor)
+
+# Pull data from TPR set
+
+query_tpr_aggregated = DataPath(datastore, """SELECT A3 \
+, CASE WHEN employees IS NULL THEN 0 ELSE 1 END AS tpr_match \
+, CASE WHEN employees IS NOT NULL THEN employees \
+WHEN A1=0 and company_type='C' THEN 16 \
+WHEN A1=0 and company_type='F' THEN 25 \
+WHEN A1=0 and company_type='I' THEN 8 \
+WHEN A1=0 and company_type='L' THEN 34 \
+WHEN A1=0 and company_type='P' THEN 28.5 \
+WHEN A1=0 and company_type='S' THEN 33 \
+WHEN A1=1 and company_type='C' THEN 189 \
+WHEN A1=1 and company_type='E' THEN 259.5 \
+WHEN A1=1 and company_type='F' THEN 156 \
+WHEN A1=1 and company_type='I' THEN 153 \
+WHEN A1=1 and company_type='L' THEN 261 \
+WHEN A1=1 and company_type='P' THEN 683 \
+WHEN A1=1 and company_type='S' THEN 579 \
+WHEN A1=1 THEN 200 \
+WHEN A1=0 THEN 16 \
+ELSE NULL END AS employees \
+, CASE WHEN scheme_start_year IS NOT NULL THEN scheme_start_year \
+WHEN A1=0 and company_type='P' THEN 2008 \
+WHEN A1=1 and company_type='C' THEN 2004 \
+WHEN A1=1 and company_type='F' THEN 2009 \
+WHEN A1=1 and company_type='E' THEN 2008 \
+WHEN A1=0 and company_type='I' THEN 2012 \
+WHEN A1=0 and company_type='C' THEN 2012 \
+WHEN A1=1 and company_type='P' THEN 1999 \
+WHEN A1=0 and company_type='L' THEN 2007 \
+WHEN A1=1 and company_type='S' THEN 2003 \
+WHEN A1=1 and company_type='I' THEN 2001 \
+WHEN A1=0 and company_type='F' THEN 2008 \
+WHEN A1=1 and company_type='L' THEN 2010 \
+WHEN A1=0 THEN 2011 \
+WHEN A1=1 THEN 2004 \
+ELSE NULL END AS scheme_start_year \
+, COALESCE(company_type,'X') as company_type \
+, company_status \
+FROM ( \
+SELECT A3 \
+, A1 \
+, MAX(D10) AS employees \
+, MIN(YEAR(D6)) AS scheme_start_year \
+, MAX(SUBSTRING(D12,1,1)) AS company_type \
+, MAX(D8) AS company_status \
+FROM  \
+    (SELECT A3 \
+    , A1 \
+    FROM STG.PT_A \
+    WHERE A2<'2019-09-01' \
+    ) A  \
+LEFT JOIN  \
+    (SELECT D15, D10, D6, D12, D8 \
+    FROM STG.PT_D \
+    ) B  \
+ON A.A3=B.D15 \
+GROUP BY A3, A1 \
+) c \
+""")
+tabular_tpr_aggregated = Dataset.Tabular.from_sql_query(query_tpr_aggregated, query_timeout=10)
+quality_tpr_aggregated = tabular_tpr_aggregated.to_pandas_dataframe()
+
+# Join TPR data to model set
+quality_model_set = pd.merge(quality_model_set,  \
+                  quality_tpr_aggregated, \
+                  left_on='account_id', \
+                  right_on='A3', \
+                  how='left')
+
+# Create dummy variables for company type
+company_type=pd.get_dummies(quality_model_set['company_type'],prefix='comp_type')
+quality_model_set = quality_model_set.merge(company_type, left_index=True, right_index=True)
+
+# Create year account created variable
+quality_model_set['cohort'] = quality_model_set['account_created'].dt.year
+
+# Alter tpr_scheme_start_year to years_since_tpr_signup
+quality_model_set['years_since_tpr_signup']=quality_model_set['cohort'].astype(int)-quality_model_set['scheme_start_year']
+
+# Function for new company flag
+
+def fn_new_company(row):
+    if row['months_since_sign_up2']<=6 :
+        val=1
+    else:
+        val=0
+    return val
+
+quality_model_set['new_company']=quality_model_set.apply(fn_new_company,axis=1)
+
+# Function for early adopter flag
+
+def fn_early_adopter(row):
+    if row['account_created']<=datetime.datetime(2017,7,1) :
+        val=1
+    else:
+        val=0
+    return val
+quality_model_set['early_adopter']=quality_model_set.apply(fn_early_adopter,axis=1)
+
+
+
+
+print(quality_model_set)
+
+
+
+
+
+
+# Sample data for modelling
+quality_sample_data=quality_model_set.sample(50000)
+
+# Only keep relevant variables and rename accordingly
+model_cols_to_keep=['A3','levy_split','completed_commitment','previous_12mon_commitments', \
+                    'apprenticeship_level','apprentice_age','funded_by_levy_transfer', \
+                    'occupation_1','occupation_2','occupation_3','occupation_7', \
+                    'occupation_13','occupation_14','occupation_15','occupation_17','occupation_20','occupation_22', \
+                    'occupation_24','months_since_sign_up2','employees','scheme_start_year','comp_type_C','comp_type_E', \
+                    'comp_type_F','comp_type_I','comp_type_L','comp_type_P','comp_type_S','comp_type_X','tpr_match', \
+                    'new_company','early_adopter','years_since_tpr_signup','company_status','commitment_date']
+quality_sample_data = quality_sample_data[model_cols_to_keep]
+quality_sample_data.columns = ['account_id','levy_non_levy','completed_commitment','previous_12mon_commitments', \
+                     'apprenticeship_level','apprentice_age','funded_by_levy_transfer','occupation_1','occupation_2', \
+                     'occupation_3','occupation_7','occupation_13','occupation_14','occupation_15','occupation_17', \
+                     'occupation_20','occupation_22','occupation_24','as_months_since_sign_up','employees', \
+                     'tpr_scheme_start_year','comp_type_C','comp_type_E','comp_type_F','comp_type_I','comp_type_L', \
+                     'comp_type_P','comp_type_S','comp_type_X','tpr_match','new_company','early_adopter', \
+                     'years_since_tpr_signup','company_status','commitment_date']
+
+# Take logs to standardise the scale
+quality_sample_data['log_employees'] = np.log2(quality_sample_data['employees']+1)
+
+# Remove outliers and non matched tpr data and tpr closed companies
+quality_sample_data2 = quality_sample_data[(quality_sample_data.employees <=20000) & (quality_sample_data.tpr_match ==1) & (quality_sample_data.company_status==3)]
+#quality_sample_data2.to_parquet('C:/Users/rober/Documents/Atos/data/completion_model_sample_data_subset.parquet')
+
+
+
+print(quality_sample_data2)
 
 
 run = Run.get_context()
