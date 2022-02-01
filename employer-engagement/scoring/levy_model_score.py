79c2742a8498bdf2e267e3853f507263c943d46a
robertsonrichard354 <robertsonrichard@hotmail.com>
2022-02-01 13:01:37 +0000

    Update dates

--- employer-engagement/scoring/levy_model_score.py
+++ employer-engagement/scoring/levy_model_score.py
@@ -27,7 +27,7 @@ levy_score_set = tabular_levy_score_set.to_pandas_dataframe()
 
 # change to today
 # months since apprenticeship account sign-up
-levy_score_set["months_since_sign_up"] = (pd.Timestamp(2022,01,30) - pd.to_datetime(levy_score_set["A2"]))/ np.timedelta64(1, "M")
+levy_score_set["months_since_sign_up"] = (pd.Timestamp(2022,1,30) - pd.to_datetime(levy_score_set["A2"]))/ np.timedelta64(1, "M")
 # make the months since sign-up discrete for analysis purposes
 levy_score_set["months_since_sign_up2"] =levy_score_set["months_since_sign_up"].apply(np.floor)
 
