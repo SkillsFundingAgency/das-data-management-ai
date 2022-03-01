1d372a1926b5cc1e0615f949bcd0081f0db289d5
robertsonrichard354 <93527142+robertsonrichard354@users.noreply.github.com>
2022-03-01 21:02:06 +0000

    Update attach_levy_train.py
    add versioning train

--- ml_service/pipelines/attach_levy_train.py
+++ ml_service/pipelines/attach_levy_train.py
@@ -30,7 +30,7 @@ def get_levy_train(aml_workspace: Workspace, aml_compute: str, pipeline_run_conf
     levy_model_train_pipeline = Pipeline(workspace=aml_workspace, steps=levy_model_train_step_sequence)
     levy_model_train_pipeline.validate()
 
-    pipeline_endpoint = PipelineEndpoint.get(workspace=aml_workspace, name="levy-model-train-pipeline")
+    pipeline_endpoint = PipelineEndpoint.get(workspace=aml_workspace, name="levy-model-train-pipeline", pipeline_version="0")
 
     levy_model_train_pipeline_run = experiment.submit(pipeline_endpoint, levy_model_train_pipeline,regenerate_outputs=True)
 
