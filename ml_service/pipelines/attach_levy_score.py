900127c73a0ce816cf7d1e31e3e712745a5db067
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-03-02 13:41:47 +0000

    new Endpoint name

--- ml_service/pipelines/attach_levy_score.py
+++ ml_service/pipelines/attach_levy_score.py
@@ -32,10 +32,20 @@ def get_levy_score(aml_workspace: Workspace, aml_compute: str, pipeline_run_conf
     # Publish pipeline to AzureML
     levy_model_score_published_pipeline = levy_model_score_pipeline.publish('levy-model-score-pipeline')
     
-    pipeline_endpoint= PipelineEndpoint.get(workspace=aml_workspace,
+    pipeline_endpoint = PipelineEndpoint.publish(
+                                     workspace=aml_workspace,
+                                     name="levy_score_model_endpoint",
+                                     pipeline=published,
+                                     description="Endpoint to Levy Score Model",
+                                               )
+    pipeline_endpoint.add_default(levy_model_score_published_pipeline)
+    
+    
+
+    #pipeline_endpoint= PipelineEndpoint.get(workspace=aml_workspace,
                                                     name="levy_model_score_endpoint"
                                            )                                               
-    pipeline_endpoint.add_default(levy_model_score_published_pipeline)
+    #pipeline_endpoint.add_default(levy_model_score_published_pipeline)
 
     #pipeline_endpoint = PipelineEndpoint.get(workspace=aml_workspace, name="levy-model-score-pipeline")
     #pipeline_endpoint.add(levy_model_score_pipeline)
