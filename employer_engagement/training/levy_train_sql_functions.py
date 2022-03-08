1b290a24700180ebd23da8332f45073b38800c23
robertsonrichard354 <robertsonrichard@hotmail.com>
2022-03-08 22:17:23 +0000

    parameter passing

--- employer_engagement/training/levy_train_sql_functions.py
+++ employer_engagement/training/levy_train_sql_functions.py
@@ -24,19 +24,19 @@ datastore = Datastore.get(aml_workspace, datastore_name='datamgmtdb')
     
     # return levy_model_accounts
 
-# def levy_train_01_accounts(top_x: str) :
-    # query_levy_accounts = DataPath(datastore, "SELECT top " + str(top_x) + "A1, A2, A3, CASE WHEN CAST(A2 AS DATE)<cast('2017-07-01' as date) THEN 1 ELSE 0 END AS early_adopter FROM PDS_AI.PT_A WHERE A1=0 ORDER BY RAND()"
-    # tabular_levy_accounts = Dataset.Tabular.from_sql_query(query_levy_accounts, query_timeout=10)
-    # levy_model_accounts = tabular_levy_accounts.to_pandas_dataframe()
-    
-    # return levy_model_accounts
-
 def levy_train_01_accounts(top_x: str) :
-    query_levy_accounts = DataPath(datastore, """SELECT top {} A1, A2, A3, CASE WHEN CAST(A2 AS DATE)<cast('2017-07-01' as date) THEN 1 ELSE 0 END AS early_adopter FROM PDS_AI.PT_A WHERE A1=0 ORDER BY RAND()""".format(top_x)
+    query_levy_accounts = DataPath(datastore, "SELECT top " + str(top_x) + "A1, A2, A3, CASE WHEN CAST(A2 AS DATE)<cast('2017-07-01' as date) THEN 1 ELSE 0 END AS early_adopter FROM PDS_AI.PT_A WHERE A1=0 ORDER BY RAND()")
     tabular_levy_accounts = Dataset.Tabular.from_sql_query(query_levy_accounts, query_timeout=10)
     levy_model_accounts = tabular_levy_accounts.to_pandas_dataframe()
     
     return levy_model_accounts
+
+# def levy_train_01_accounts(top_x: str) :
+    # query_levy_accounts = DataPath(datastore, "SELECT top {} A1, A2, A3, CASE WHEN CAST(A2 AS DATE)<cast('2017-07-01' as date) THEN 1 ELSE 0 END AS early_adopter FROM PDS_AI.PT_A WHERE A1=0 ORDER BY RAND()".format(top_x)
+    # tabular_levy_accounts = Dataset.Tabular.from_sql_query(query_levy_accounts, query_timeout=10)
+    # levy_model_accounts = tabular_levy_accounts.to_pandas_dataframe()
+    
+    # return levy_model_accounts
 
 
 # def levy_train_02_levy_model_set_2018_2019_part1() :
