b8f26c583303d19481ddbe8686c3696cff868bc1
robertsonrichard354 <robertsonrichard@hotmail.com>
2022-03-10 20:35:02 +0000

    additional log tests

--- employer_engagement/training/levy_model_train.py
+++ employer_engagement/training/levy_model_train.py
@@ -36,12 +36,13 @@ try:
     # Create model build data into dataframe
     # Create df with all accounts and early adopter flag
     levy_model_accounts=levy_train_functions.levy_train_01_accounts(7)
+    run.log('Success','01 Accounts Success')
+except Exception:
+    run.log('EXCEPTION','01 Accounts Exception')
 
-    print (levy_model_accounts)
-
+try:
     # select account_ids into list 
     account_list = levy_model_accounts['A3'].tolist()
-    print(account_list)
 
     #Remove brackets from list
     sql_account_list=str(account_list)[1:-1]
@@ -68,29 +69,53 @@ try:
 
     # make the months since sign-up discrete for analysis purposes
     levy_model_set["months_since_sign_up2"] =levy_model_set["months_since_sign_up"].apply(np.floor)
+    run.log('Success','01a Accounts Success')
+except Exception:
+    run.log('EXCEPTION','01a Accounts Exception')
 
+try:
     # 2018/2019 cohort Part 1
     levy_model_set_2018_2019_part1=levy_train_functions.levy_train_02_levy_model_set_2018_2019_part1(sql_account_list)
+    run.log('Success','02 Commitments 2018/19 Part 1 Success')
+except Exception:
+    run.log('EXCEPTION','02 Commitments 2018/19 Part 1 Exception')
 
+try:
     # 2018/2019 cohort Part 2
     levy_model_set_2018_2019_part2=levy_train_functions.levy_train_03_levy_model_set_2018_2019_part2(sql_account_list)
+    run.log('Success','03 Commitments 2018/19 Part 2 Success')
+except Exception:
+    run.log('EXCEPTION','03 Commitments 2018/19 Part 2 Exception')
 
-
+try:
     # 2019/2020 cohort Part 1
     levy_model_set_2019_2020_part1=levy_train_functions.levy_train_04_levy_model_set_2019_2020_part1(sql_account_list)
+    run.log('Success','04 Commitments 2019/20 Part 1 Success')
+except Exception:
+    run.log('EXCEPTION','04 Commitments 2019/20 Part 1 Exception')
 
+try:
     # 2018/2019 cohort Part 2
     levy_model_set_2019_2020_part2=levy_train_functions.levy_train_05_levy_model_set_2019_2020_part2(sql_account_list)
+    run.log('Success','05 Commitments 2019/20 Part 2 Success')
+except Exception:
+    run.log('EXCEPTION','05 Commitments 2019/20 Part 2 Exception')
 
-
+try:
     # 2022 cohort Part 1
     levy_model_set_2022_part1=levy_train_functions.levy_train_06_levy_model_set_2022_part1(sql_account_list)
+    run.log('Success','06 Commitments 2022 Part 1 Success')
+except Exception:
+    run.log('EXCEPTION','06 Commitments 2022 Part 1 Exception')
 
     # 2022 cohort Part 2
     levy_model_set_2022_part2=levy_train_functions.levy_train_07_levy_model_set_2022_part2(sql_account_list)
+    run.log('Success','07 Commitments 2022 Part 2 Success')
+except Exception:
+    run.log('EXCEPTION','07 Commitments 2022 Part 2 Exception')
 
+try:
     # join both parts together for all time periods
-
     universe_commitments_2019 = pd.merge(levy_model_set_2018_2019_part1, \
                       levy_model_set_2018_2019_part2, \
                       left_on=['A3'], \
@@ -123,10 +148,18 @@ try:
 
     # Fill commitments with 0 if missing
     levy_model_set = levy_model_set.fillna(0)
+    run.log('Success','08 Account manipulation Success')
+except Exception:
+    run.log('EXCEPTION','08 Account manipulation Exception')
 
+try:
     # TPR Data
     levy_tpr_aggregated=generic_train_functions.generic_01_tpr(sql_account_list)
+    run.log('Success','09 TPR Success')
+except Exception:
+    run.log('EXCEPTION','09 TPR Exception')
 
+try:
     # Join TPR data to model set
     levy_model_set = pd.merge(levy_model_set, \
                       levy_tpr_aggregated, \
@@ -154,10 +187,18 @@ try:
         return val
 
     levy_model_set['new_company']=levy_model_set.apply(fn_new_company,axis=1)
+    run.log('Success','10 TPR manipulation Success')
+except Exception:
+    run.log('EXCEPTION','10 TPR manipulation Exception')
 
+try:
     # SIC Data
     sic_aggregated=generic_train_functions.generic_02_sic(sql_account_list)
+    run.log('Success','11 SIC Success')
+except Exception:
+    run.log('EXCEPTION','11 SIC Exception')
 
+try:
     # Match SIC data onto accounts
     levy_model_set = pd.merge(levy_model_set, \
                       sic_aggregated, \
@@ -185,7 +226,11 @@ try:
     levy_model_set2 = levy_model_set[(levy_model_set.employees <=20000) & (levy_model_set.tpr_match ==1) & (levy_model_set.company_status ==3)]
 
     levy_model_set3=levy_model_set2
+    run.log('Success','12 SIC manipulation Success')
+except Exception:
+    run.log('EXCEPTION','12 SIC manipulation Exception')
 
+try:
     levy_model_set3.rename(columns = {'A1':'levy_non_levy', 'A3':'account_id', 'months_since_sign_up2':'as_months_since_sign_up'}, inplace = True)
 
     #Drop any unnecessary rows
@@ -198,10 +243,10 @@ try:
 
     #Drop rows with any null values
     levy_model_set4 = levy_model_set3.dropna() 
-
-    print(levy_model_set4)
+    run.log('Success','13 Model data prep Success')
 except Exception:
-    run.log('data exception occurred','data exception occured')
+    run.log('EXCEPTION','13 Model data prep Exception')
+
     
 try:
     #Split dataset into independent and dependent variables
@@ -213,10 +258,19 @@ try:
     X_train = X_train_acc.drop(['account_id'], axis=1)
     X_test = X_test_acc.drop(['account_id'], axis=1)
 
+    run.log('Success','14 Model train test Success')
+except Exception:
+    run.log('EXCEPTION','14 Model train test Exception')
+
+try:
     #Set parameters of xgboost regressor
     xg_reg = xgb.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.05,
     max_depth = 5, alpha = 10, n_estimators = 700, verbosity = 2, eval_metric='rmse')
+    run.log('Success','15 Model build Success')
+except Exception:
+    run.log('EXCEPTION','15 Model build Exception')
 
+try:
     #Train our model using the training dataset
     xg_reg.fit(X_train,y_train)
 
@@ -234,7 +288,16 @@ try:
     shap_values = explainer.shap_values(X_train)
     plt.clf()
     shap.summary_plot(shap_values, X_train, feature_names=X.drop(['account_id'], axis=1).columns, plot_type="bar", show=True)
+    run.log('Success','16 Model stats Success')
+except Exception:
+    run.log('EXCEPTION','16 Model stats Exception')
+    # Logging Stats
+    run.log('R2',r2)
+    run.log('Reduced R2',reduced_r2_output)
+    run.log('RMSE',rmse)
+    run.log_image('Shap Plot', plot=plt)
 
+try:
     # Save the trained model in the outputs folder
     os.makedirs('outputs', exist_ok=True)
     model_file = os.path.join('outputs', 'levy_model.pkl')
@@ -244,14 +307,12 @@ try:
     Model.register(workspace=aml_workspace, \
                    model_path = model_file, \
                    model_name = 'levy_model')
-
-    # Logging
-    run.log('R2',r2)
-    run.log('Reduced R2',reduced_r2_output)
-    run.log('RMSE',rmse)
-    run.log_image('Shap Plot', plot=plt)
+    run.log('Success','17 Model save Success')
 except Exception:
-    run.log('model exceptionoccurred','model exception occured')
+    run.log('EXCEPTION','17 Model save Exception')
 
-
-
+try:
+    run.get_all_logs(destination='outputs')
+    run.log('Success','18 Output Logs')
+except Exception:
+    run.log('EXCEPTION','18 Output Logs')
