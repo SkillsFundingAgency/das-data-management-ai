afd0728d0dbe652041278245e14385ab4c939eb1
David Goodyear <david.goodyear@digital.education.gov.uk>
2022-03-17 21:41:33 +0000

    Fixng output

--- azure/Set-AzRoleAssignment.ps1
+++ azure/Set-AzRoleAssignment.ps1
@@ -8,7 +8,9 @@ Param(
 ### TODO: Get the variables passed in from the template outputs
 $SubscriptionId = (Get-AzContext).Subscription.Id 
 #$resourceGroupName = "das-test-datamgmtai-rg"
+Write-Output $resourceGroupName
 #$StorageAccountName = "dastestdatamgmtaistr"
+Write-Output $StorageAccountName
 
 ### TODO: Do this better so its not duplicated script for each role assignment
 # Get the MI IDs
@@ -18,7 +20,7 @@ $mlclusterid = (Get-AzADServicePrincipal -DisplayName das-test-datamgmtai-ml/com
 # Check if role assignment exists and create if needed
 $RoleAssignments = Get-AzRoleAssignment -Scope /subscriptions/$SubscriptionId/resourcegroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$StorageAccountName | Where-Object {$_.RoleDefinitionName -eq "Storage Blob Data Contributor" -and $_.ObjectId -eq $mlid} 
 
-Write-Host $RoleAssignments
+Write-Output $RoleAssignments
 
 if ($RoleAssignments.Length -eq 0) {
     New-AzRoleAssignment -ObjectId $mlid`
@@ -29,7 +31,7 @@ if ($RoleAssignments.Length -eq 0) {
 # Do the same for the cluster principal
 $RoleAssignments = Get-AzRoleAssignment -Scope /subscriptions/$SubscriptionId/resourcegroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$StorageAccountName | Where-Object {$_.RoleDefinitionName -eq "Storage Blob Data Contributor" -and $_.ObjectId -eq $mlclusterid} 
 
-Write-Host $RoleAssignments
+Write-Output $RoleAssignments
 
 if ($RoleAssignments.Length -eq 0) {
     New-AzRoleAssignment -ObjectId $mlclusterid`
