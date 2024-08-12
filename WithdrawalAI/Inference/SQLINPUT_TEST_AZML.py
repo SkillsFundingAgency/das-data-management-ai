76890fac404f714964ff51beced043290c4ea0d6
manthonyEviden <150236123+manthonyEviden@users.noreply.github.com>
2024-08-12 15:37:32 +0100

    Test Env
    Update YML

---
+++ WithdrawalAI/Inference/SQLINPUT_TEST_AZML.py
@@ -0,0 +1,114 @@
+import pandas as pd
+
+import pyodbc 
+import sqlalchemy
+import azure.identity
+
+from azure.identity import DefaultAzureCredential
+
+
+import argparse
+import json
+
+
+import json
+
+import argparse
+parser=argparse.ArgumentParser('Dummy_Routine.py')
+parser.add_argument("--creds",action='store',dest='creds',help='Credentials (JSON) required to call Azure SQL',default="../../AUTH_TOKEN/AUTH.json")
+
+import time
+import datetime
+currtime=datetime.datetime.now()
+print(currtime)
+DD=currtime.day
+MM=currtime.month
+YYYY=currtime.year
+
+hh=currtime.hour
+mm=currtime.minute
+
+ts_formatted=f"WithdrawalRateAIOutput_{mm}_{hh}_{DD}_{MM}_{YYYY}.csv"
+
+print(ts_formatted)
+
+date=currtime.date()
+ttime=currtime.time()
+
+
+args=parser.parse_args()
+#jf=open(args.creds)
+#creds=json.load(jf)
+
+#SERVER=creds['server']
+#USERNAME=creds['user']
+
+#DATABASE=creds['database']
+
+#AUTHENTICATION=creds['auth']
+
+from azureml.core.run import Run
+from azureml.core import Dataset, Datastore, Model
+from azureml.data.datapath import DataPath
+from azureml.core.experiment import Experiment
+from azureml.core.workspace import Workspace
+
+
+
+driver="ODBC Driver 18 for SQL Server"
+
+from azureml.core import Workspace
+from azureml.core.compute import ComputeTarget
+from azureml.pipeline.steps import PythonScriptStep
+from azureml.pipeline.core import Pipeline, PipelineData, StepSequence, PublishedPipeline
+from azureml.core.runconfig import RunConfiguration
+from azureml.pipeline.core import PipelineEndpoint
+import azureml.core
+import os
+from azureml.data.datapath import DataPath
+from azureml.core import Workspace, Datastore, Dataset, ComputeTarget, Experiment, ScriptRunConfig, Environment, Model
+from azureml.core.run import Run
+ 
+# Set up config of workspace and datastore
+
+
+aml_workspace = Run.get_context().experiment.workspace
+datastore = Datastore.get(aml_workspace, datastore_name='datamgmtdb')
+
+
+#print(run)
+
+
+def QueryDB(dummyvar=1000):
+    query=DataPath(datastore,"SELECT TOP({}) * FROM ASData_PL.Comt_Apprenticeship".format(1000)) # just to show how it works with the data
+    tabular_obj=Dataset.Tabular.from_sql_query(query,query_timeout=3600)
+    df_pandas=tabular_obj.to_pandas_dataframe()
+    return df_pandas.copy(deep=True)
+
+df_in=QueryDB()
+
+
+
+df_BDT_ALLOC=df_in.sample(frac=0.5,replace=False,random_state=42)
+
+
+df_sample_1=df_in.sample(frac=0.4,replace=False,random_state=42)
+df_sample_not1=df_in.drop(index=df_sample_1.index)
+df_sample_3=df_sample_not1.sample(frac=0.2,random_state=42)
+df_sample_2=df_sample_not1.drop(index=df_sample_3.index)
+
+df_out=df_in.copy(deep=True)
+df_out['Predicted_Withdrawal']=0
+df_out['Predicted_Withdrawal'].iloc[df_BDT_ALLOC.index]=1
+df_out['EmailClassification']="DUD"
+df_out['EmailClassification'].iloc[df_sample_1.index]="Email A"
+df_out['EmailClassification'].iloc[df_sample_2.index]='Email B'
+df_out['EmailClassification'].iloc[df_sample_3.index]="Early Years"
+print(df_out['EmailClassification'].value_counts())
+
+df_out['ApprenticeshipId']=df_out['Id']
+df_out=df_out[['ApprenticeshipId','EmailClassification','Predicted_Withdrawal']]
+
+df_out.to_csv("./{}.csv".format(ts_formatted))
+
+exit(0)
---
+++ ml_service/pipelines/WithdrawalAI_Pipeline.py
@@ -0,0 +1,86 @@
+import azureml.core
+import os
+from azureml.core import Workspace, Datastore, Dataset, ComputeTarget, Experiment, ScriptRunConfig, Environment, Model
+from azureml.core.run import Run
+from azureml.core.runconfig import RunConfiguration
+from azureml.pipeline.core import Pipeline, PipelineData, StepSequence, PublishedPipeline
+from azureml.pipeline.steps import PythonScriptStep
+from azureml.core import Workspace
+from azureml.core.compute import ComputeTarget
+
+
+from ml_service.util.env_variables import Env
+from ml_service.util.manage_environment import get_environment
+from Pipeline_Piece_AZMLDummy import get_WithdrawalAIModel
+from ml_service.pipelines.attach_levy_score import get_levy_score
+
+
+
+
+
+# Set up all environment details
+e = Env()
+subscription_id = os.environ.get("SUBSCRIPTION_ID")
+workspace_name = os.environ.get("WORKSPACE_NAME")
+resource_group = os.environ.get("RESOURCE_GROUP")
+
+# Get Azure machine learning workspace
+aml_workspace = Workspace.get(
+name=e.workspace_name,
+subscription_id=e.subscription_id,
+resource_group=e.resource_group,
+)
+
+# Create experiment if it doesn't exist
+experiment = Experiment(aml_workspace, "WithdrawalAI")
+
+# Add compute information to variable
+aml_compute = aml_workspace.compute_targets["cpucluster"]
+
+# Set up experiment folder
+experiment_folder = 'WithdrawalAI'
+ 
+# Create a Python environment for the experiment (from a .yml file)
+environment = Environment.from_conda_specification("environment", experiment_folder + "/conda_env.yml")
+# Register the environment 
+environment.register(workspace=aml_workspace)
+registered_env = Environment.get(aml_workspace, 'environment')
+# Create a new runconfig object for the pipeline
+pipeline_run_config = RunConfiguration()
+# Use the compute you created above. 
+pipeline_run_config.target = aml_compute
+# Assign the environment to the run configuration
+pipeline_run_config.environment = registered_env
+
+
+def get_WithdrawalAIModel(aml_workspace: Workspace, aml_compute: str, pipeline_run_config: str, experiment: str) :
+    # non_levy_model train step
+    train_source_dir="./WithdrawalAI/Inference/" # path is a dud
+    WithdrawalAIModel_step = PythonScriptStep(
+        name='WithdrawalAIModel',
+        script_name="SQLINPUT_TEST_AZML.py",
+        compute_target=aml_compute,
+        runconfig=pipeline_run_config,
+        source_directory=train_source_dir)
+
+
+    # Create sequence of steps for model train
+    WithdrawalAIModel_step_sequence = StepSequence(steps = [WithdrawalAIModel_step])
+
+    # Create pipeline
+    WithdrawalAIModel_pipeline = Pipeline(workspace=aml_workspace, steps=WithdrawalAIModel_step_sequence)
+    WithdrawalAIModel_pipeline.validate()
+
+    WithdrawalAIModel_pipeline_run = experiment.submit(WithdrawalAIModel_step_sequence,regenerate_outputs=True)
+
+    # RunDetails(pipeline_run).show()
+    WithdrawalAIModel_pipeline_run.wait_for_completion()
+
+    # Publish pipeline to AzureML
+    WithdrawalAIModel_published_pipeline = WithdrawalAIModel_pipeline.publish('WithdrawalAI-model-train-pipeline')
+    
+    return
+
+
+#Create pipelines for withdrawal rate model
+get_WithdrawalAIModel(aml_workspace,aml_compute,pipeline_run_config,experiment)
