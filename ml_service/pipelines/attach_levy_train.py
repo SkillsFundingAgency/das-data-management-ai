6f07d615a798df72ad2e22abcf61d15a81774764
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2024-08-18 19:35:12 +0100

    Revert "Adding  pipeline Parameter"

--- ml_service/pipelines/attach_levy_train.py
+++ ml_service/pipelines/attach_levy_train.py
@@ -2,7 +2,6 @@ from azureml.core import Workspace
 from azureml.core.compute import ComputeTarget
 from ml_service.util.env_variables import Env
 from azureml.pipeline.steps import PythonScriptStep
-from azureml.pipeline.core import PipelineParameter
 from azureml.pipeline.core import Pipeline, PipelineData, StepSequence, PublishedPipeline
 from azureml.core.runconfig import RunConfiguration
 from azureml.pipeline.core import PipelineEndpoint
@@ -12,13 +11,12 @@ from azureml.core import Workspace, Datastore, Dataset, ComputeTarget, Experimen
 from azureml.core.run import Run
 from ml_service.util.manage_environment import get_environment
 
-def get_levy_train(aml_workspace: Workspace, aml_compute: str, pipeline_run_config: str, pipeline_param: str) :
+def get_levy_train(aml_workspace: Workspace, aml_compute: str, pipeline_run_config: str, experiment: str) :
     # levy_model train step
     train_source_dir="./employer_engagement/training"
     levy_model_train_step = PythonScriptStep(
         name='levy_model_train',
         script_name="levy_model_train.py",
-        arguments=["--param1", pipeline_param],
         compute_target=aml_compute,
         runconfig=pipeline_run_config,
         source_directory=train_source_dir,
