9e235e00b2b1db71c7e47e558204ca2815c13642
manthonyEviden <150236123+manthonyEviden@users.noreply.github.com>
2024-08-22 14:10:35 +0100

    move model registry ahead of run config

--- ml_service/pipelines/WithdrawalAI_Pipeline.py
+++ ml_service/pipelines/WithdrawalAI_Pipeline.py
@@ -40,12 +40,7 @@ environment = Environment.from_conda_specification("environment", experiment_fol
 # Register the environment 
 environment.register(workspace=aml_workspace)
 registered_env = Environment.get(aml_workspace, 'environment')
-# Create a new runconfig object for the pipeline
-pipeline_run_config = RunConfiguration()
-# Use the compute you created above. 
-pipeline_run_config.target = aml_compute
-# Assign the environment to the run configuration
-pipeline_run_config.environment = registered_env
+
 
 #Register model - even though its inference we need to tell AzureML that there's an AI model involved otherwise things start crashing
 Model.register(workspace=aml_workspace,
@@ -54,6 +49,14 @@ Model.register(workspace=aml_workspace,
                tags={'pretrained':'inception'},
                description='Withdrawal AI model'
                )
+# Create a new runconfig object for the pipeline
+pipeline_run_config = RunConfiguration()
+# Use the compute you created above. 
+pipeline_run_config.target = aml_compute
+# Assign the environment to the run configuration
+pipeline_run_config.environment = registered_env
+
+
 
 print("**************************************")
 print("START: Check if a model is registered correctly prior to job start")
