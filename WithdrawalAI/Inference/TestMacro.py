09cb596e711d75f541f7ec803595ca4c41fc576d
manthonyEviden <150236123+manthonyEviden@users.noreply.github.com>
2024-08-27 10:13:10 +0100

    update

---
+++ WithdrawalAI/Inference/TestMacro.py
@@ -0,0 +1,54 @@
+import xgboost as xgb
+import pandas as pd
+import numpy as np
+import os
+import pickle
+def main():
+    print("HELLO WORLD")
+    if(1==1):
+        #create a dummy dataframe with same variables as the dummy df used to configure the dummy xgb model
+        v1=np.random.randn(100)
+        v2=np.random.randn(100)
+        v3=np.random.randn(100)
+        v4=np.random.randn(100)
+        df_t=pd.DataFrame({
+            'v1':v1,
+            'v2':v2,
+            'v3':v3,
+            'v4':v4
+        })
+        print(os.getcwd())
+        print(os.system("pip show xgboost"))
+    try:
+        loaded_model=None    
+        with open('dummy_model.pkl', 'rb') as f:
+            loaded_model = pickle.load(f)
+    
+        print(loaded_model)
+    except Exception as e:
+        print("Exception: ",e)
+
+    try:
+        from azureml.core import Model,Workspace
+        # this will fail currently until the workspace IDs are filled
+        ws=Workspace.get(
+            subscription_id="",
+            resource_group="",
+            name= ""
+        )
+        modctr=0
+
+        for model in Model.list(ws):
+            if("prod" in model.name.lower()):
+                print("iteration:{}".format(modctr))
+                print(model.name,model.version)
+                modctr+=1
+        print("END MODEL READ")
+    except Exception as e:
+        print("MOD QUERY ERROR: {}".format(e))
+
+ 
+    return
+
+if __name__=='__main__':
+    main()
