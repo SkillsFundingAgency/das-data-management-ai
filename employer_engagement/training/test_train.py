e447f10dc4ae1152ba69ecd6e57388ea2a7cd1b6
manthonyEviden <150236123+manthonyEviden@users.noreply.github.com>
2024-10-04 12:18:14 +0100

    remove extraneous files from the autoencoder directory

--- employer_engagement/training/test_train.py
+++ employer_engagement/training/test_train.py
@@ -267,6 +267,7 @@ try:
     os.system('cp -r -a -v ./ML_Models/Download_Manifest/Dummy_Autoencoder/.* ./ML_Models/Models/Dummy_AE/') # additional block to move hidden files
     
     print("ASSIGNING ADDITIONAL PERMS TO AE BINARIES")
+    os.system("rm -r -v ./ML_Models/Models/Dummy_AE/ONSData") # for some reason the ONSData is being downloaded into the Autoencoder directory - remove it
     os.system('chmod 700 ./ML_Models/Models/Dummy_AE/* ') # permissions on the Autoencoder binaries
     os.system('chmod 700 ./ML_Models/Models/Dummy_AE/.* ') # permissions on the autoencoder binaries' hidden files
     os.system('chmod 700 -R ./ML_Models/Models/Dummy_AE') # permissions on everything in this directory
