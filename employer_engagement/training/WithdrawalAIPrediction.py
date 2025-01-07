40c31bb429a2fe39857a9aa534c107c57ef7eedf
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2025-01-07 17:02:43 +0000

    removed validation report

--- employer_engagement/training/WithdrawalAIPrediction.py
+++ employer_engagement/training/WithdrawalAIPrediction.py
@@ -80,9 +80,9 @@ except Exception as E:
     run.log("EXCEPTION 2",f'Exception: {E}')
 
 try:
-    df_in_AIMod,validation_report=sql_interface.ExtractView()
+    df_in_AIMod=sql_interface.ExtractView()
     run.log('INFO 9","Queried Datamart OK')
-    run.log("INFO 10","Extraction verification report: {}".format(str(validation_report)))
+    #run.log("INFO 10","Extraction verification report: {}".format(str(validation_report)))
 except Exception as E:
     run.log("EXCEPTION 3",f'Exception: {E}')
     pass
